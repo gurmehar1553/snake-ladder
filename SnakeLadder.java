dd5b1630abd7f017d5084a8958b5d2a5a10bfde8
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-23 14:07:44 +0530

    Added:UC2 dice number obtained

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,9 @@ public class SnakeLadder{
 	public static void main(String[] args){
 		System.out.println("Welcome");
 		int START_POS = 0;
+		
+		int diceNum = (int) (Math.ceil(Math.random()*10) % 6)+1;
+		System.out.println(diceNum);
 	}
 
 }
